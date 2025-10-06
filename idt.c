6e9a0ef9ba702656d42fccc2ad988bd1c20dacb9
Yash Patel <yp969803@gmail.com>
2025-10-06 18:06:18 +0530

    feat: pic remap

--- idt.c
+++ idt.c
@@ -1,4 +1,5 @@
 #include "idt.h"
+#include "io.h"
 
 // ISR for keyboard interrupt
 extern void isr_keyboard();      // ISR defined in assembly (or change name to isr_keyboard)
@@ -21,6 +22,34 @@ static inline void load_idt(void) {
     asm volatile ("lidt %0" : : "m"(idt_ptr));  // load the IDT
 }
 
+static inline pic_remap(int offset1, int offset2) {
+    unsigned char a1, a2;
+
+    // Save masks
+    a1 = inb(0x21);   // get master mask
+    a2 = inb(0xA1);   // get slave mask
+
+    // Start initialization sequence (cascade mode)
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+
+    // Set vector offset
+    outb(0x21, offset1);  // Master PIC vector offset
+    outb(0xA1, offset2);  // Slave PIC vector offset
+
+    // Tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+    outb(0x21, 0x04);
+    outb(0xA1, 0x02);
+
+    // Set environment info
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+
+    // Restore saved masks
+    outb(0x21, a1);
+    outb(0xA1, a2);
+}
+
 void idt_init(void) {
     // zero out IDT
     for (int i = 0; i < IDT_SIZE; ++i) {
@@ -31,4 +60,5 @@ void idt_init(void) {
 
     // finally load the IDT
     load_idt();
+    pic_remap(0x20, 0x28);  // Master at 0x20–0x27, Slave at 0x28–0x2F
 }
---
+++ io.h
@@ -0,0 +1,13 @@
+inline unsigned char inb(unsigned short port) {
+    unsigned char ret;
+    __asm__ __volatile__ ("inb %1, %0"
+                          : "=a"(ret)
+                          : "Nd"(port));
+    return ret;
+}
+
+inline void outb(unsigned short port, unsigned char val) {
+    __asm__ __volatile__ ("outb %0, %1"
+                          :
+                          : "a"(val), "Nd"(port));
+}
