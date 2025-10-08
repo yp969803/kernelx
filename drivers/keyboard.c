6bcdea6724c7a6d9973814f4d64327876ffb4c1c
Yash Patel <yp969803@gmail.com>
2025-10-09 00:06:17 +0530

    fix: add missing EXTENDED_SCANCODE definition and comment for key release in keyboard handler

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -4,6 +4,10 @@
 
 #define KEYBOARD_DATA_PORT 0x60
 
+#define EXTENDED_SCANCODE 0xe0
+
+// Key release = scancode + 0x80
+
 void keyboard_handler_c() {
     uint8_t scancode = inb(KEYBOARD_DATA_PORT);
 
