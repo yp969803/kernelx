99db5908ec2bcac2806c7e19cff780e16ec3f407
Yash Patel <yp969803@gmail.com>
2025-10-09 18:29:11 +0530

    feat: enhance keyboard handler with extended scancode definitions and keymap initialization

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -6,10 +6,25 @@
 
 #define EXTENDED_SCANCODE 0xe0
 
+#define ENTER 0x1C
+#define BACKSPACE 0x0E
+#define ESCAPE 0x01
+#define LEFT_SHIFT 0x2A 	
+#define CAPS_LOCK 0x3A
+
 // Key release = scancode + 0x80
+
+const char keymap[128] = {
+    0,  27, '1','2','3','4','5','6','7','8','9','0','-','=','\b',
+    '\t','q','w','e','r','t','y','u','i','o','p','[',']','\n',0,
+    'a','s','d','f','g','h','j','k','l',';','\'','`',0,'\\',
+    'z','x','c','v','b','n','m',',','.','/',0,'*',0,' ',0};
 
 void keyboard_handler_c() {
     uint8_t scancode = inb(KEYBOARD_DATA_PORT);
 
-    vga_put_char(scancode & 0x0F);
+    if(scancode & 0x80 || scancode >= 128 || keymap[scancode] == 0 || keymap[scancode]==27 || scancode == 0xe0) {
+        return;
+    }
+    vga_put_char(keymap[scancode]);
 }
