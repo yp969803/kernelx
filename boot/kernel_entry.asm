0d8dda849c889146b564a3346a112a9600d6840f
Yash Patel <yp969803@gmail.com>
2025-10-10 22:39:43 +0530

    rfac: kernel entry

--- boot/kernel_entry.asm
+++ boot/kernel_entry.asm
@@ -1,10 +1,21 @@
 [bits 32]
 
+section .multiboot               ;according to multiboot spec
+        dd 0x1BADB002            ;set magic number for
+                                 ;bootloader
+        dd 0x0                   ;set flags
+
 global _start      ; make this the entry point for linker
 extern main        ; tell assembler that 'main' is in another file
 
 section .text
 _start:
+    cli
+    mov ebp, stack_space 
+    mov esp, ebp
     call main      ; call kernel main in C
-.hang:
-    jmp .hang      ; infinite loop so CPU doesn't run into garbage
+    hlt
+
+section .bss
+resb 16000                        ;16KB for stack
+stack_space:
