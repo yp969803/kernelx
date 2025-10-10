d52b986171bd4bebc6b3d6b0df06c44b500380b1
Yash Patel <yp969803@gmail.com>
2025-10-10 23:01:21 +0530

    fix: update .gitignore and reorganize kernel entry for multiboot compliance

--- boot/kernel_entry.asm
+++ boot/kernel_entry.asm
@@ -1,9 +1,10 @@
 [bits 32]
 
-section .multiboot               ;according to multiboot spec
-        dd 0x1BADB002            ;set magic number for
-                                 ;bootloader
-        dd 0x0                   ;set flags
+section .multiboot
+    align 4
+    dd 0x1BADB002
+    dd 0x00010003
+    dd -(0x1BADB002 + 0x00010003)
 
 global _start      ; make this the entry point for linker
 extern main        ; tell assembler that 'main' is in another file
