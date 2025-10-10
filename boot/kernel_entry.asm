51c64e4b2048e10901360809a035bbcc055df62c
Yash Patel <yp969803@gmail.com>
2025-10-10 23:01:32 +0530

    fix: correct multiboot section alignment and flags for compliance

--- boot/kernel_entry.asm
+++ boot/kernel_entry.asm
@@ -1,10 +1,11 @@
 [bits 32]
 
-section .multiboot
-    align 4
-    dd 0x1BADB002
-    dd 0x00010003
-    dd -(0x1BADB002 + 0x00010003)
+
+section .multiboot               ;according to multiboot spec
+        dd 0x1BADB002            ;set magic number for
+                                 ;bootloader
+        dd 0x0                   ;set flags
+        dd - (0x1BADB002 + 0x0)  ;set checksum
 
 global _start      ; make this the entry point for linker
 extern main        ; tell assembler that 'main' is in another file
