1e68c01dad55e4bdeb5c2b8686558295e4b60239
Yash Patel <yp969803@gmail.com>
2025-10-12 13:10:01 +0530

    fix: add memory_copy function for improved memory handling

--- kernel/mem.c
+++ kernel/mem.c
@@ -14,3 +14,13 @@ void* memmove(void* dest, const void* src, size_t n) {
 
     return dest;
 }
+
+
+void memory_copy(void *src, void *dest, size_t nbytes) {
+    uint8_t* d = (uint8_t*)dest;
+    const uint8_t* s = (const uint8_t*)src;
+    int i;
+    for (i = 0; i < nbytes; i++) {
+        *(d + i) = *(s + i);
+    }
+}
--- kernel/mem.h
+++ kernel/mem.h
@@ -4,3 +4,4 @@
 #include <stdint.h>
 
 void* memmove(void* dest, const void* src, size_t n);
+void memory_copy(void *src, void *dest, size_t nbytes);
