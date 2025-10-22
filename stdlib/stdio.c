46f5aeb234f1fb36ae75a66be4742908d4aa6449
Yash Patel <yp969803@gmail.com>
2025-10-22 20:22:18 +0530

    feat: implement kprintf function for formatted output in stdio

---
+++ stdlib/stdio.c
@@ -0,0 +1,40 @@
+#include "stdio.h"
+
+
+void kprintf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    for (const char *p = format; *p; p++){
+        if (*p != '%') {
+            vga_put_char(*p);
+            continue;
+        }
+        p++;
+
+        switch (*p) {
+            case 'c': {
+                char c = (char)va_arg(args, int);
+                vga_put_char(c);
+                break;
+            }
+            case 's': {
+                const char *s = va_arg(args, const char *);
+                vga_print_string(s);
+                break;
+            }
+            case 'd': {
+                int d = va_arg(args, int);
+                vga_print_int(d);
+                break;
+            }
+            case 'x': {
+                unsigned int x = va_arg(args, unsigned int);
+                vga_print_hex(x);
+                break;
+            }
+            default:
+                vga_put_char('%');
+                vga_put_char(*p);
+        }
+    }
+}
---
+++ stdlib/stdio.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdarg.h>
+#include <stdint.h>
+#include "../drivers/vga.h"
+
+void kprintf(const char *format, ...);
