3a4986c567b24eb65f5214f180d019abff0612ab
Yash Patel <yp969803@gmail.com>
2025-10-23 17:05:49 +0530

    feat: add timer header file and fix floating-point division in timer functions

--- drivers/timer.c
+++ drivers/timer.c
@@ -1,4 +1,4 @@
-#include <stdint.h>
+#include "timer.h"
 #include "../cpu/io.h"
 
 
@@ -8,14 +8,14 @@
 
 
 void pit_set_timer(uint32_t milliseconds) {
-    uint16_t count = (uint16_t)(PIT_FREQ * milliseconds / 1000.0);
+    uint16_t count = (uint16_t)(PIT_FREQ * milliseconds / 1000);
     outb(PIT_COMMAND, 0x36); 
     outb(PIT_CHANNEL0, count & 0xFF); 
     outb(PIT_CHANNEL0, (count >> 8) & 0xFF); 
 }
 
 void pit_set_delay(uint32_t milliseconds) {
-    uint16_t count = (uint16_t)(PIT_FREQ * milliseconds / 1000.0);
+    uint16_t count = (uint16_t)(PIT_FREQ * milliseconds / 1000);
     outb(PIT_COMMAND, 0x38);
     outb(PIT_CHANNEL0, count & 0xFF);
     outb(PIT_CHANNEL0, (count >> 8) & 0xFF);
---
+++ drivers/timer.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdint.h>
+
+void pit_set_timer(uint32_t milliseconds);
+void pit_set_delay(uint32_t milliseconds);
