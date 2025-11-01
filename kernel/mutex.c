8f277ec1ccb10d005e8cd1132da03b0f8654b8cc
Yash Patel <yp969803@gmail.com>
2025-11-01 12:22:09 +0530

    feat: implement spin lock functionality with initialization, lock, and unlock operations

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -79,3 +79,34 @@ void mutex_unlock(mutex *m)
         m->count = 1;
     }
 }
+
+spin_lock *new_spin_lock(void)
+{
+    spin_lock *m = kmalloc(sizeof(spin_lock));
+    m->count     = 1;
+    m->owner     = NULL;
+    return m;
+}
+
+void spink_lock_init(spin_lock *m)
+{
+    m->count = 1;
+    m->owner = NULL;
+}
+
+void spin_lock_lock(spin_lock *m)
+{
+    uint32_t expected = 1;
+    while (!atomic_compare_exchange(&m->count, &expected, 0))
+        ;
+    m->owner = current_task_TCB;
+}
+
+void spin_lock_unlock(spin_lock *m)
+{
+    if (m->owner != current_task_TCB) {
+        return;
+    }
+    m->owner = NULL;
+    m->count = 1;
+}
--- kernel/mutex.h
+++ kernel/mutex.h
@@ -15,7 +15,16 @@ typedef struct mutex {
     mutex_wait_node *wait_queue_tail;
 } mutex;
 
+typedef struct spin_lock {
+    uint32_t count;
+    thread_control_block *owner;
+} spin_lock;
+
 mutex *new_mutex(void);
 void mutex_init(mutex *m);
 void mutex_lock(mutex *m);
 void mutex_unlock(mutex *m);
+spin_lock *new_spin_lock(void);
+void spin_lock_init(spin_lock *m);
+void spin_lock_lock(spin_lock *m);
+void spin_lock_unlock(spin_lock *m);
